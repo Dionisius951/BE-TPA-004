d661a1effd053f8f51d4ba11ead6f1a153b15a7d
Dionisius951 <dionisius951@gmail.com>
2023-11-12 20:57:06 +0700

    menambahkan query pada table customers

---
+++ Insert Data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Customers ( Name, Email, Address, Phone)
+VALUES
+    ('Dionisius', 'dionisius@gmail.com', 'Jl. Sudirman 123', '08123456789'),
+    ('Andri', 'Andri@gmail.com', 'Jl. Thamrin 456', '08765432123'),
+    ('Farel', 'farel@gmail.com', 'Jl. Gatot Subroto 789', '08111223344'),
+    ('Adam', 'adam@gmail.com', 'Jl. Diponegoro 101', '08778889900'),
+    ('Hirzi', 'hirzi@gmail.com', 'Jl. Teuku Umar 202', '08122334455'),
+    ('Auzan', 'auzan@gmail.com', 'Jl. Kusuma Bangsa 303', '08887776666'),
+    ('Dewa', 'dewa@gmail.com', 'Jl. Darmo 404', '08133445566'),
+    ('Paulus', 'paulus@gmail.com', 'Jl. Sisingamangaraja 505', '08771234567'),
+    ('Gidion', 'gidion@gmail.com', 'Jl. Pemuda 606', '08155667788'),
+    ('Bryan', 'bryanr@gmail.com', 'Jl. Majapahit 707', '08889990000');
